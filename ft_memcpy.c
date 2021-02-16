cae5f4811fd0a397e359e2a5ff0380b74dc7bb26
Larissa <larifaeld@gmail.com>
2021-02-16 19:03:39 -0300

    porfavorvai

---
+++ ft_memcpy.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltrivinh <ltrivinh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/10 15:02:32 by ltrivinh          #+#    #+#             */
+/*   Updated: 2021/02/11 13:59:50 by ltrivinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// copia um bloco de dados (int,char) de uma src para um dst
+// prototipo void * memcpy(void * destination, const void * source, size_t num);
+//  void *memcpy(void *dest, const void *src, size_t n);
---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltrivinh <ltrivinh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/11 14:08:48 by ltrivinh          #+#    #+#             */
+/*   Updated: 2021/02/16 13:38:06 by ltrivinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include "libft.h"
+
+void		*ft_memset(void *phrase, int letter, size_t size)
+{
+    unsigned char   *ph;
+    size_t          cont;
+
+    cont = 0;
+    ph = phrase;
+    while (size > cont)
+    {
+        ph[cont] = letter;
+        cont++;
+    }
+    return (phrase);
+}*/
